a37c263bb8ea41caad254c60f9130115c1f2fbee
Gabriel Perdue <gnperdue@me.com>
2014-11-30 10:40:04 -0600

    Timestamp backups

--- installnix.sh
+++ installnix.sh
@@ -4,20 +4,22 @@
 # If they do, archvie them into _bck files. Links to
 # a `.emacs.d` doesn't seem to work for emacs.
 
+DAT=`date -u +%s`
+
 # Handle `.emacs`
 if [ -e $HOME/.emacs ]; then
-  if [ -e $HOME/.bak_emacs ]; then
-    rm -vf $HOME/.bak_emacs
+  if [ -e $HOME/.bak_emacs_$DAT ]; then
+    rm -vf $HOME/.bak_emacs_$DAT
   fi
-  mv -v $HOME/.emacs $HOME/.bak_emacs
+  mv -v $HOME/.emacs $HOME/.bak_emacs_$DAT
 fi
 # cp ${PWD}/emacs.emacs $HOME/.emacs
 
 # Handle `.emacs.d`
 if [ -e $HOME/.emacs.d ]; then
-  if [ -e $HOME/.bak_emacs.d ]; then
-    rm -rvf $HOME/.bak_emacs.d
+  if [ -e $HOME/.bak_emacs.d_$DAT ]; then
+    rm -rvf $HOME/.bak_emacs.d_$DAT
   fi
-  mv -v $HOME/.emacs.d $HOME/.bak_emacs.d
+  mv -v $HOME/.emacs.d $HOME/.bak_emacs.d_$DAT
 fi
 cp -rv ${PWD}/emacs.d $HOME/.emacs.d
