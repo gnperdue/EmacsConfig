4443af39e2c26e0cfb7dc8982c3194c2142d2b32
Gabriel Perdue <gnperdue@me.com>
2014-11-30 14:40:22 -0600

    Some customizations

---
+++ emacs.d/gnp-noexternals.el
@@ -0,0 +1,11 @@
+;; Remove scrollbars and toolbars - the menubar is useful sometimes. But, as I
+;; usually run `emacs -nw` and keep it in a terminal, the rest is useless.
+;; (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Customize windmove a bit
+(global-set-key (kbd "C-c <down>") 'windmove-down)
+(global-set-key (kbd "C-c <up>") 'windmove-up)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <left>") 'windmove-left)
