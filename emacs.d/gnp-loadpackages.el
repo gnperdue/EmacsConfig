5fdd3c08c5e43f82d4726ae4e4617508773bd996
Gabriel Perdue <gnperdue@me.com>
2014-12-20 14:59:25 -0600

    fix web-mode color issues for working with html by manually setting colors for various pieces of the code (e.g., html tag faces, etc.)

--- emacs.d/gnp-loadpackages.el
+++ emacs.d/gnp-loadpackages.el
@@ -41,5 +41,18 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;; For some reason, I need to set all my web-mode colors by hand.
+;; See http://web-mode.org, because I may need to set css, json, etc.
+;; Also note, the Apple Terminal only "supports" 8 colors
+;;  black, red, green, yellow, blue, magenta, cyan, white
+;; iTerm2 is an option to consider...
+(set-face-attribute 'web-mode-doctype-face nil :foreground "magenta")
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "cyan")
+(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "yellow")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "green")
+(set-face-attribute 'web-mode-html-attr-value-face nil :foreground "blue")
+(set-face-attribute 'web-mode-html-attr-equal-face nil :foreground "yellow1")
+(set-face-attribute 'web-mode-html-tag-custom-face nil :foreground "cyan")
+;; JS
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
